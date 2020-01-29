4392972552df69a7c904aa203cf5f5fa4fdfabf5
wangjie <17054214@qq.com>
2020-01-29 14:18:39 +0800

    分类左侧roots 数据展示

--- w-sleeve/pages/category/category.js
+++ w-sleeve/pages/category/category.js
@@ -1,6 +1,7 @@
 // pages/category/category.js
 import {getSystemSize} from "../../utils/system";
 import {px2rpx} from "../../miniprogram_npm/lin-ui/utils/util";
+import {Categories} from "../../models/categories";
 
 Page({
 
@@ -8,13 +9,41 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    categories: Object,
+    defaultRootId: String
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: async function (options) {
+      this.setDynamicSegmentHeight()
+      this.initCategoryData()
+  },
+
+  async initCategoryData() {
+    const categories = new Categories()
+    this.data.categories = categories
+    await categories.getAll()
+    const roots = categories.getRoots()
+    const defaultRoot = this.getDefaultRoot(roots)
+    const currentSubs = categories.getSubs(defaultRoot.id)
+    this.setData({
+      roots,
+      currentSubs,
+      currentBannerImg: defaultRoot.img
+    })
+  },
+
+  getDefaultRoot(roots) {
+    let defaultRoot = roots.find(s=>s.id===this.data.defaultRootId)
+    if (!defaultRoot && roots.length > 0) {
+      defaultRoot = roots[0]
+    }
+    return defaultRoot
+  },
+
+  async setDynamicSegmentHeight() {
     const height = await getWindowHeightRpx()
     const h = height - 60 - 20 - 2
     this.setData({
